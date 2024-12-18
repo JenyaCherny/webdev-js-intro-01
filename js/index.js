4ccd0458a1f531805e615fc2f3b12ef29dc7a572
JenyaCherny <jenya.cherny@gmail.com>
2024-12-17 21:13:03 -0500

    completed experimenting in index.js and commented all out

--- js/index.js
+++ js/index.js
@@ -1,3 +1,21 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+/*const twenty = 20;
+console.log(twenty);
+
+let bestFriend = "Victor";
+console.log(bestFriend);
+
+bestFriend = "Alina";
+console.log(bestFriend);
+
+const city = Toronto;
+console.log(city);
+
+let age = 48;
+console.log(age);
+
+age = 49;
+console.log(age);
+*/
